fc1d83b344ca9831ff33485d96d98f41404746bb
realShirai <sebisch2727@hotmail.com>
2021-11-08 19:33:17 +0100

    vsc to vss

---
+++ QuizGame/QuizGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuizGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
